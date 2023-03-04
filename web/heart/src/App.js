9ee707dd95cfa5dcc1809acded5ad830fcf70ab5
1ceOrb <aaronmzhu@gmail.com>
2023-03-04 17:33:35 -0600

    imported NextUI

--- web/heart/src/App.js
+++ web/heart/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react'
+import { NextUIProvider } from '@nextui-org/react';
 import * as ReactDOM from 'react-dom';
 import Glass from './components/Glass'
 
@@ -8,8 +9,12 @@ function App() {
   return (
     <div className="app">
     <div className="wrapper">
-      <h1>How About Them Apples</h1>
+      <h1>Input Biological Factors</h1>
       <form>
+      </form>
+      <h1>Input Socioeconomic Factors</h1>
+      <form>
+
       </form>
     </div>
   </div>
