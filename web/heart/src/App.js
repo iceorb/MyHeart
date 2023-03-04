5748d8148d628fc936e25ddb3442831896b1f1d5
1ceOrb <aaronmzhu@gmail.com>
2023-03-04 17:36:48 -0600

    build nextui

--- web/heart/src/App.js
+++ web/heart/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-import { NextUIProvider } from '@nextui-org/react';
 import * as ReactDOM from 'react-dom';
 import Glass from './components/Glass'
 
-function App() {
-  return (
-    <div className="app">
-    <div className="wrapper">
-      <h1>Input Biological Factors</h1>
-      <form>
-      </form>
-      <h1>Input Socioeconomic Factors</h1>
-      <form>
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from '@nextui-org/react';
 
-      </form>
-    </div>
-  </div>
+function App({ Component }) {
+  // 2. Use at the root of your app
+  return (
+    <NextUIProvider>
+      <Component />
+    </NextUIProvider>
   );
 }
 
