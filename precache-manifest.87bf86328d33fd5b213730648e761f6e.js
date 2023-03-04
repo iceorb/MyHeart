f8b8e6196eff8007a1d7252bd3a0a72b5975257a
1ceOrb <aaronmzhu@gmail.com>
2023-03-04 17:11:49 -0600

    Updates

---
+++ precache-manifest.87bf86328d33fd5b213730648e761f6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a214c8f5139e34b5ea14",
+    "url": "/madhacks2023/static/css/main.af0f70e8.chunk.css"
+  },
+  {
+    "revision": "a214c8f5139e34b5ea14",
+    "url": "/madhacks2023/static/js/main.a214c8f5.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/madhacks2023/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "970b3542874f8148f8dc",
+    "url": "/madhacks2023/static/js/2.970b3542.chunk.js"
+  },
+  {
+    "revision": "577e7132ea1e74bc4dda",
+    "url": "/madhacks2023/static/js/runtime~main.577e7132.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/madhacks2023/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "09caa98e17e9a264f1be9755bad79d04",
+    "url": "/madhacks2023/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/madhacks2023/precache-manifest.e0120e5c3d86aa02ac28d0166c5a808d.js"
+  "/madhacks2023/precache-manifest.87bf86328d33fd5b213730648e761f6e.js"
 );
 
 workbox.clientsClaim();
