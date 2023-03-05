b453160ae9e07367a5d60888eb159ed1d9138aec
1ceOrb <aaronmzhu@gmail.com>
2023-03-05 03:37:29 -0600

    removed themes

--- web/src/App.js
+++ web/src/App.js
@@ -11,35 +11,6 @@ import Nav from './components/Nav';
 
 function App({ Component }) {
 
-  const theme = createTheme({
-    type: "dark", // it could be "light" or "dark"
-    theme: {
-      colors: {
-        // brand colors
-        primaryLight: '$green200',
-        primaryLightHover: '$green300',
-        primaryLightActive: '$green400',
-        primaryLightContrast: '$green600',
-        primary: '#4ADE7B',
-        primaryBorder: '$green500',
-        primaryBorderHover: '$green600',
-        primarySolidHover: '$green700',
-        primarySolidContrast: '$white',
-        primaryShadow: '$green500',
-  
-        gradient: 'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
-        link: '#5E1DAD',
-  
-        // you can also create your own color
-        myColor: '#ff4ecd'
-  
-        // ...  more colors
-      },
-      space: {},
-      fonts: {}
-    }
-  })
-
   const handleSubmit = (data) => {
     console.log(data);
     fetch('https://localhost:8080/bio-data', {
