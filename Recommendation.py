5a24768404d7a89f74af471824bd325ac15e33f6
Robin Yang <robyang33@gmail.com>
2023-03-05 11:20:35 -0600

    Recommendation and modified sdoh

---
+++ Recommendation.py
@@ -0,0 +1,68 @@
+import pandas as pd
+
+def recommend(file,heart, smoke, alc, phys, sleep):
+
+    df = pd.read_csv(file)
+    df = df[df['HeartDisease'] == 0].reset_index()
+ 
+
+    col = ['Smoking','AlcoholDrinking','PhysicalActivity','SleepTime']
+    input = [smoke, alc, phys, sleep]
+    output = [0,0,0,0]
+    mean = [0,0,0,0]
+    std = [0,0,0,0]
+
+    i = 0
+    for s in col:
+        mean[i] = df[s].mean()
+        std[i] = df[s].std()
+      
+        i = i+1
+
+    print(mean)
+    print(std)
+    #smoke
+    if input[0] > mean[0] + (std[0]*1.5):
+        output[0] = 2
+    elif input[0] > mean[0] + (std[0]):
+        if heart == 0:
+            output[0] = 1
+        else:
+            output[0] = 2
+    #alc
+    if input[1] > mean[1] + (std[1]*1.5):
+        output[1] = 2
+    elif input[1] > mean[1] + (std[1]):
+        if heart != 0:
+            output[1] = 1
+    #phys
+    if input[2] < mean[2] - (std[2]*1.5):
+        if heart == 2:
+            output[2] = 2
+        elif heart == 1:
+            output[2] = 1
+    elif input[2] < mean[2] - (std[2]):
+        if heart != 0:
+            output[2] = 1
+    #sleep
+    if input[3] < mean[3] - (std[3]*1.5):
+        if heart == 0:
+            output[3] = 1
+        else:
+            output[3] = 2
+    elif input[3] < mean[3] - (std[3]):
+        if heart == 1:
+            output[3] = 1
+        elif heart == 2:
+            output[3] = 2
+ 
+    return [heart, output[0], output[1],output[2],output[3]]
+
+   
+
+        
+
+
+
+
+
--- sdoh.py
+++ sdoh.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_validate,GridSearchCV,ParameterGrid
+from sklearn.model_selection import cross_validate,GridSearchCV,ParameterGrid, cross_val_score
 from sklearn.model_selection import KFold,RepeatedKFold
 import graphviz
 df1 = pd.read_csv('full_part1.csv')
@@ -42,6 +42,7 @@ forest_reg= RandomForestRegressor(n_estimators=1,bootstrap=False).fit(X_train,Y_
 Y_train_predict = forest_reg.predict(X_train)    # Make predictions on training data
 Y_test_predict = forest_reg.predict(X_test)      # Make predictions on testing data
 
+print(forest_reg.score(X_test,Y_test))
 
 
 MSE = np.square(np.subtract(Y_train,Y_train_predict)).mean() 
@@ -56,27 +57,60 @@ RMSE = math.sqrt(MSE)
 print("Root Mean Square Error:\n")
 print(RMSE)
 
-number_of_trees = [1,10,25,50]
+def rfr_model(X, y):
+# Perform Grid-Search
+    gsc = GridSearchCV(
+        estimator=RandomForestRegressor(),
+        param_grid={
+            'n_estimators': (10, 50, 100, 200),
+        },
+        cv=5, scoring='root_mean_squared_error', verbose=0,n_jobs=-1)
+    
+    grid_result = gsc.fit(X, y)
+    best_params = grid_result.best_params_
+    
+    rfr = RandomForestRegressor(n_estimators=best_params["n_estimators"],random_state=False, verbose=False)
+    scores = cross_val_score(rfr, X, y, cv=10, scoring='neg_mean_absolute_error')
 
-### MAKE EDITS ABOVE HERE ###
+    return scores
 
-opt_dict = {'n_estimators':number_of_trees,'bootstrap':['True']}
-# kfold = KFold(n_splits=5,random_state=seed,shuffle=True)
-kfold = RepeatedKFold(n_splits=5,
-                      n_repeats=5)
+from sklearn.metrics import mean_squared_error
 
+# Load your dataset into X and y arrays
 
-CV = GridSearchCV(total,# 1. the model whose hyperparamter is being optimized right now
-                  opt_dict,   # 2. a dictionary of values that we want the grid search to use
-                  cv=kfold,   # 4. k-fold cross-validation strategy is used to define training and validation splits (note this is separate from test splits) to be used for each grid point
-                  return_train_score=True,
-                  scoring=['neg_mean_squared_error','r2','neg_mean_absolute_error'], # 5. the performance metrics to be reported at each grid point specified in opt_dict
-                  refit='neg_mean_squared_error')
+# Define the number of folds for cross-validation
+k = 20
 
-# perform grid search
+# Define the random forest regressor model
+rf = RandomForestRegressor(n_estimators=100)
 
-CV = CV.fit(X_train,Y_train)
+# Initialize the KFold cross-validation object
+kf = KFold(n_splits=k, shuffle=True, random_state=42)
 
+# Initialize an array to store the mean squared errors for each fold
+mse_scores = []
 
-# print results
-print(CV.best_params_)
+# Loop over the KFold object to perform cross-validation
+for train_idx, test_idx in kf.split(X):
+    # Split the data into training and testing sets
+    X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+    y_train, y_test = Y[train_idx], Y[test_idx]
+    
+    # Fit the random forest regressor model on the training data
+    rf.fit(X_train, y_train)
+    
+    # Predict the target variable for the testing data
+    y_pred = rf.predict(X_test)
+    
+    # Calculate the mean squared error for this fold
+    mse = mean_squared_error(y_test, y_pred)
+    
+    # Add the mean squared error to the list of scores
+    mse_scores.append(mse)
+
+# Calculate the mean and standard deviation of the mean squared errors
+mean_mse = np.mean(mse_scores)
+std_mse = np.std(mse_scores)
+
+# Print the mean and standard deviation of the mean squared errors
+print("Mean squared error: {:.3f} (± {:.3f})".format(mean_mse, std_mse))
